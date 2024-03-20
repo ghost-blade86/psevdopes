909d6d12b39ddaf940faeebdb857d11c05ecd363
ghost-blade86 <164106616+ghost-blade86@users.noreply.github.com>
2024-03-21 02:20:37 +0300

    Add files via upload

---
+++ Словари и множества.py
@@ -0,0 +1,16 @@
+my_list = ["банан", "апельсин", "мандарин", "кокос", "персик", "яблоко"]
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = "арбуз"
+print(my_list)
+
+my_dict = {"клинок": "blade", "автомат": "machine"  }
+print(my_dict)
+print(my_dict["клинок"])
+my_dict["клинок"] = "sword"
+print(my_dict)
+my_dict["9К51"] = "град"
+print(my_dict)
+
