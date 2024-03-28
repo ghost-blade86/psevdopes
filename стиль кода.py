21e4a31c3c131baaa8de2cde66affbd416605e09
ghost-blade86 <164106616+ghost-blade86@users.noreply.github.com>
2024-03-28 16:52:35 +0300

    Add files via upload

---
+++ стиль кода.py
@@ -0,0 +1,9 @@
+while 1 > 0:
+    number = int(input('введите число:'))
+    if number % 2 == 0:
+        print('число четное')
+        continue
+    else:
+        print('нечетное число')
+    print('меня не забыли')
+print('я за циклом')
